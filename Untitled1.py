27027c76d7408a96c6afd60e56da46c989c5ba9c
Bolineni Rahul <80504885+Luhar8@users.noreply.github.com>
2022-01-25 11:26:42 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **RAHUL
+
+# *I am interested in data science because it will going to be hot jobs of 21st century
+
+# ### Code about addition of two numbers
+
+# In[1]:
+
+
+a = 10
+b = 20
+print(a+b)
+
+
+# ---
+
+# List of Car Companies
+# <ul>
+#     <li>BMW</li>
+#     <li>Audi</li>
+#     <li>Benz</li>
+# </ul>
+
+# <a href = "https://www.coursera.org/in">Coursera</a>
+
+# <s>Good bye everyone</s>
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
